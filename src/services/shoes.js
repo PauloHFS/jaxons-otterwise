81cb3a12209684267e2c4df875d05385effbdce6
Paulo Hernane <paulohernane10@gmail.com>
2022-01-14 10:05:56 -0300

    set username on .env

--- src/services/shoes.js
+++ src/services/shoes.js
@@ -10,7 +10,7 @@ const getAllLikes = async () => {
   const { data, error } = await supabase
     .from('liked_shoe')
     .select('*')
-    .eq('user_name', 'paulo_hernane');
+    .eq('user_name', `${process.env.REACT_APP_USERNAME}`);
   if (error) console.error(error);
   return data;
 };
@@ -19,7 +19,7 @@ const setLikeAs = async (likeState, liked_id, shoe_id) => {
   const { data, error } = await supabase.from('liked_shoe').upsert({
     id: liked_id,
     shoe_id: shoe_id,
-    user_name: 'paulo_hernane',
+    user_name: `${process.env.REACT_APP_USERNAME}`,
     is_liked: likeState,
   });
   if (error) console.error(error);
@@ -30,7 +30,7 @@ const isShoeLiked = async shoe_id => {
   const { data, error } = await supabase
     .from('liked_shoe')
     .select('*')
-    .eq('user_name', 'paulo_hernane')
+    .eq('user_name', `${process.env.REACT_APP_USERNAME}`)
     .eq('shoe_id', `${shoe_id}`);
   if (error) console.error(error);
   return data;
